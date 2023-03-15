29275dd9b267d73b111d0041b734786c35ddcb32
McCoy Becker <mccoybecker@gmail.com>
2023-03-15 00:29:04 -0400

    Local.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -23,7 +23,6 @@ require("user.autocommands")
 -- For LaTeX editing + LuaSnip.
 vim.g.tex_flavor = "latex"
 vim.g.vimtex_view_method = "skim"
-vim.g.vimtex_quickfix_mode = 0
 vim.g.conceallevel = 1
 vim.g.tex_conceal = "abdmg"
 
